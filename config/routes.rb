0bfd45c5ec764f9df5ff081b4d95babaef0d0849
Chris Poulton <cvpoulton@gmail.com>
2014-03-31 17:12:39 -0600

    Removed comments from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -55,8 +55,4 @@ CSCI3308FinalProject::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  
-  # This is Chris Poulton
-  # shimmy shimmy coco what pass it to me now
-
 end
