b7c088e9a63c9186ceb9f343a61815c04a2ebd11
cvpoulton <cvpoulton@gmail.com>
2014-04-05 19:38:00 -0300

    Added unique usernames and updated walldate on account creation - CP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :interests, :last_name, :password, :quotes, :walldate
+  attr_accessible :username, :first_name, :interests, :last_name, :password, :quotes, :walldate
   has_many :posts
   has_many :friendships
   has_many :pending_friendships
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140402221604) do
+ActiveRecord::Schema.define(:version => 20140405215606) do
 
   create_table "friendships", :force => true do |t|
     t.integer  "user_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20140402221604) do
     t.datetime "walldate"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "username"
   end
 
 end
