d5494f6d58615d3ca999911cf9d21647362e6593
cvpoulton <cvpoulton@gmail.com>
2014-04-05 21:01:29 -0300

    Added header to show what user is logged in - CP

--- app/controllers/viewing_controller.rb
+++ app/controllers/viewing_controller.rb
@@ -2,12 +2,16 @@ class ViewingController < ApplicationController
   def newsfeed
     if not session.has_key?(:login_id) # If not logged in then redirect to login page
       redirect_to '/account/login'
+    else
+      @current_user = User.find(session[:login_id]) # Set current user for header
     end
   end
 
   def profile
     if not session.has_key?(:login_id) # If not logged in then redirect to login page
       redirect_to '/account/login'
+    else
+      @current_user = User.find(session[:login_id]) # Set current user for header
     end
   end
 end
