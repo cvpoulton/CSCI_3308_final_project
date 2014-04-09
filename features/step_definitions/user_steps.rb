7b05017d4ab8209a984ae3a83146b58b8fff20f1
cvpoulton <cvpoulton@gmail.com>
2014-04-08 22:54:14 -0300

    Added failing tests - CP

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -3,3 +3,19 @@ Given /the following users exist/ do |users_table|
     User.create!(user)
   end
 end
+
+Given /I am logged in as "(.*)" with password "(.*)"$/ do |user, pass|
+  visit(login_url)
+  fill_in("Username", :with => user)
+  fill_in("Password", :with => pass)
+  click_link_or_button("Login")
+  @current_user = User.find(:all, :conditions => {:username => user, :password => pass})[0]
+end
+
+Then /^my interests should be "(.*?)"$/ do |interests|
+  assert @current_user.interests == interests
+end
+
+Then /^my quotes should be "(.*?)"$/ do |quotes|
+  assert @current_user.quotes == quotes
+end
